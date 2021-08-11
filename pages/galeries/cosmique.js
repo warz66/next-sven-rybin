6e029fe5f043bfab7d76356e5299a9d10c603a2a
Warz66 <warzox@hotmail.com>
2021-08-12 01:07:53 +0200

    first commit

--- pages/galeries/cosmique.js
+++ pages/galeries/cosmique.js
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import styles from './Cosmique.module.css'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import Masonry from 'react-masonry-css'
 
 export default function Cosmique() {
     const [galerie, setGalerie] = useState();
@@ -10,9 +12,33 @@ export default function Cosmique() {
         setGalerie($data);
     }
 
+    const breakpointColumnsObj = {
+        default: 5,
+        1500: 5,
+        1300: 4,
+        1000: 3,
+        800: 2,
+        500: 1
+      };
+
+    var images=null;
+    if(galerie) {
+        images = galerie.images.map((image, index) => {
+                    return <div key={index} style={{ position: "relative" }}>
+                                {/*<Image
+                                    src={image.pathUrl} // Route of the image file
+                                    width={image.tableau.width}
+                                    height={image.tableau.height}
+                                    alt="Your Name"
+                                />*/}
+                                <img src={image.pathUrlCache} ></img>
+                            </div>
+        });
+    }
+
     useEffect(() => {
-        axios.get("https://90.118.74.20:8000/api/galerie/svenrybin").then(response => {handleGalerie(response.data);console.log(response)});
-        //axios.get("https://localhost:8000/api/galerie/svenrybin").then(response => {handleGalerie(response.data);console.log(response)});
+        axios.get("http://90.118.74.20:8000/api/galerie/svenrybin").then(response => {handleGalerie(response.data);console.log(response)});
+        //axios.get("http://localhost:8000/api/galerie/svenrybin").then(response => {handleGalerie(response.data);console.log(response)});
     }, []);
 
     return (
@@ -23,16 +49,22 @@ export default function Cosmique() {
                     <a>Back to home</a>
                 </Link>
             </h2>
-            {galerie && galerie.images.map((image, index) =>
-                <div key={index} style={{ maxWidth : "1000px", height: "600px", position: "relative" }}>
+            {<Masonry
+                breakpointCols={breakpointColumnsObj}
+                className={styles.masonry_grid}
+                columnClassName={styles.my_masonry_grid_column}>
+                {images}
+            </Masonry>}
+            {/*galerie && galerie.images.map((image, index) =>
+                <div key={index} style={{ maxWidth : "800px", maxHeight: "600px", height : "400px", position: "relative" }}>
                     <Image
                         src={image.pathUrl} // Route of the image file
-                        width={500}
-                        height={500}
+                        layout={'fill'}
                         alt="Your Name"
+                        objectFit={'contain'}
                     />
                 </div>
-            )}
+            )*/}
             {/*<div style={{ maxWidth : "1000px", height: "600px", position: "relative" }}>
                 <Image
                     src="http://localhost:80/img/indatabase/galerie/content/61132f2d13f90_adwaita-wallpaper.jpeg" // Route of the image file
